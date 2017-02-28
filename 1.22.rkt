5789a601394512645fd6f5327eb64d32615b678f
Vladislav Pakhomov <vladislav.pakhomov@bostongene.com>
2017-02-28 23:57:11 +0300

    1.22

---
+++ 1.22.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (square x) (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+(define (runtime)
+  (current-milliseconds))
+
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime n (- (runtime) start-time))
+      false))
+
+(define (report-prime n elapsed-time)
+  (display n)
+  (display " *** ")
+  (display elapsed-time)
+  (newline))
+
+(define (search-for-primes number-from prime-count)
+  (if (> prime-count 0)
+      (if (timed-prime-test number-from)
+          (search-for-primes (+ number-from 1) (- prime-count 1))
+          (search-for-primes (+ number-from 1) prime-count))
+      false))
