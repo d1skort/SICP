3ce4056baa389e3d5c1bcfbf21721aa17d9c4ddb
Vladislav Pakhomov <vladislav.pakhomov@bostongene.com>
2017-03-08 18:27:08 +0300

    1.39

---
+++ 1.39.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+
+(define (cont-frac n d k)
+  (define (iter result i)
+    (if (= i 1)
+        (/ (n i) result)
+        (iter (+ (d (- i 1))
+                 (/ (n i) result))
+              (- i 1))))
+  (iter (d k) k))
+
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i) (if (= i 1)
+                             x
+                             (* x x)))
+             (lambda (i) (- (* i 2) 1))
+             k))
