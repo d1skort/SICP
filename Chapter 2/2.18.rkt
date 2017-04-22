f5dc9d043024f8e07dadcd91e4b93b7d6163a5da
Vladislav Pakhomov <vladislav.pakhomov@bostongene.com>
2017-04-22 22:18:24 +0300

    2.17, 2.18

---
+++ Chapter 2/2.18.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+
+(define (reverse items)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items) (cons (car items) result))))
+  (iter items nil))
